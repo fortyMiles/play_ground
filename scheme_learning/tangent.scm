dca9f4df4391d79582af4b4640ced4dee0ed43df
fortyMiles <mqgao@outlook.com>
2016-03-09 11:24:46 +0800

    finish tangent

--- scheme_learning/tangent.scm
+++ scheme_learning/tangent.scm
@@ -1,7 +1,7 @@
-(define (cont-frac n d k)
+(define (cont-frac n d k op)
   (define (cont-iter i)
     (/ (n i)
-       (+ (d i)
+       (op (d i)
 	  (if (>= i k)
 	      0
 	      (cont-iter (+ i 1))
@@ -13,13 +13,56 @@
   (cont-iter 1)
   )
 
+(define (tangent x k)
+  (define (func-x i)
+    (if (= i 1)
+	x
+	(* x x)
+	)
+    )
+
+  (define (d i)
+    (- (* 2 i) 1)
+    )
+
+  (cont-frac func-x d k -)
+  )
 
 (define (cont-frac-iter n d k)
+  (define (combine r i)
+    (/ (n i) (+ (d i) r))
+    )
   (define init-result (/ (n k) (d k)))
   (define (iter i result)
-    (if (= i 0)
+    (if (= i 1)
 	result
-	(iter (- i 1) ((d i) ))
+	(iter (- i 1) (combine result (- i 1)))
+	)
+    )
+
+  (iter k init-result)
+  )
+
+(define (euler i)
+  (let ([r (/ (- i 2) 3)])
+    (cond ((= i 1)
+	   1
+	   )
+	  ((= (remainder (- i 2) 3) 0)
+	   (* 2 (+ 1 r))
+	   )
+	  (else
+	   1
+	   )
 	)
     )
   )
+
+(define (euler-e k)
+  (+ 2
+     (cont-frac-iter (lambda (i) 1.0) euler k)
+     )
+  )
+
+
+
