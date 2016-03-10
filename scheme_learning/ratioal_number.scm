15d3a0d141f474dc0c6be874dec049f27bd7354f
fortymiles <mqgao@outlook.com>
2016-03-10 16:54:40 +0800

    test ratioal number

---
+++ scheme_learning/ratioal_number.scm
@@ -0,0 +1,32 @@
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+
+            (* (demon x) (demon y)))
+  )
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (demon y))
+               (* (numer y) (demon x))
+               )
+            (* (demon x) (denom y))
+            )
+  )
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (demon x) (denom y))
+            )
+  )
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (demon y))
+            (* (demon x) (numer y))
+            )
+  )
+
+(define (equal-rat? x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))
+     )
+  )
