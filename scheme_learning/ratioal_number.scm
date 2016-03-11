a26f286099556a8d95e72b45b0bd57a91f77f482
fortyMiles <mqgao@outlook.com>
2016-03-11 19:31:44 +0800

    add chap -2

--- scheme_learning/ratioal_number.scm
+++ scheme_learning/ratioal_number.scm
@@ -2,26 +2,26 @@
   (make-rat (+ (* (numer x) (denom y))
                (* (numer y) (denom x)))
 
-            (* (denom x) (denom y)))
+            (* (demon x) (demon y)))
   )
 
 (define (sub-rat x y)
-  (make-rat (- (* (numer x) (denom y))
-               (* (numer y) (denom x))
+  (make-rat (- (* (numer x) (demon y))
+               (* (numer y) (demon x))
                )
-            (* (denom x) (denom y))
+            (* (demon x) (denom y))
             )
   )
 
 (define (mul-rat x y)
   (make-rat (* (numer x) (numer y))
-            (* (denom x) (denom y))
+            (* (demon x) (denom y))
             )
   )
 
 (define (div-rat x y)
-  (make-rat (* (numer x) (denom y))
-            (* (denom x) (numer y))
+  (make-rat (* (numer x) (demon y))
+            (* (demon x) (numer y))
             )
   )
 
@@ -31,23 +31,3 @@
      )
   )
 
-(define (make-rat n d)
-  (let (
-	[g (gcd (abs n) (abs d))]
-	)
-    (if (< (* n d) 0)
-	(cons (* -1 (/ (abs n) g)) (/ (abs d) g))
-	(cons (/ (abs n) g) (/ (abs d) g))
-	)
-    ))
-
-(define (numer x) (car x))
-(define (denom x) (cdr x))
-
-(define (print-rat x)
-;  (newline)
-  (display (numer x))
-  (display "/")
-  (display (denom x))
-  (newline)
-  )
