d292955b18e267acce1d3beecc1646f6928b6070
fortyMiles <mqgao@outlook.com>
2016-03-16 14:23:52 +0800

    finish for each

--- scheme_learning/chap_02/2_18_reverse.scm
+++ scheme_learning/chap_02/2_18_reverse.scm
@@ -18,3 +18,63 @@
       (append (reverse (cdr L)) (list (car L)))
       )
   )
+
+(define (odd num)
+  (= (remainder num 2) 1)
+  )
+
+(define (same-singular num1 num2)
+  (= (remainder num1 2) (remainder num2 2))
+  )
+
+(define (same-parity x . L)
+  (define (parity-iter L result)
+    (if (null? L)
+        result
+        (if (same-singular x (car L))
+            (parity-iter (cdr L) (append-e result (car L)))
+            (parity-iter (cdr L) result)
+            )
+        ))
+
+  (parity-iter L (list x))
+  )
+
+(define (self-map proc items)
+  (if (null? items)
+      '()
+      (cons (proc (car items))
+            (self-map proc (cdr items))
+            )
+      )
+  )
+
+
+(define (scale L factor)
+  (map (lambda (x) (* x factor)) L)
+  )
+
+
+(define (for_each proc items)
+  (cond ((not (null? items))
+       (proc (car items))
+       (for_each proc (cdr items))
+       )
+      )
+  )
+
+(define (for-each-if proc items)
+  (if (not (null? items))
+      ((lambda () (proc (car items)) (for_each proc (cdr items))) )))
+
+(define (test-lambda n)
+  (if (> n 0)
+      ((lambda (x) (* x 2)) n)
+      )
+  )
+
+(define (self-for-each proc items)
+  (cond ((not (null? items))
+         (proc (car items))
+         (self-for-each proc (cdr items)))))
+  
