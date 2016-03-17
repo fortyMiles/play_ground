5c241847b521a8cb94b222661f25499fb2378193
fortyMiles <mqgao@outlook.com>
2016-03-17 18:38:06 +0800

    finish 2.2.3

--- scheme_learning/chap_02/map_of_tree.scm
+++ scheme_learning/chap_02/map_of_tree.scm
@@ -53,3 +53,65 @@
       (list nil)
       (let ((rest (subsets (cdr s))))
 	(append rest (map (lambda (e) (cons (car s) e)) rest)))))
+
+
+(define (filter predicate sequence)
+  (define nil '())
+  (cond ((null? sequence) nil)
+	((predicate (car sequence))
+	 (cons (car sequence)
+	       (filter predicate (cdr sequence)))
+	 )
+	(else (filter predicate (cdr sequence)))
+	))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+	  (accumulate op initial (cdr sequence))
+	  )
+      )
+  )
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      '()
+      (cons low (enumerate-interval (+ low 1) high))
+      )
+  )
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) '())
+	((not (pair? tree)) (list tree))
+	(else (append (enumerate-tree (car tree))
+		      (enumerate-tree (cdr tree))
+		      ))
+	)
+  )
+
+(define (fib n)
+  (cond ((= n 1)
+	 1)
+	((= n 0)
+	 0)
+	(else (+ (fib (- n 1)) (fib (- n 2))))
+	)
+  )
+
+(define (sum-odd-squares tree)
+  (accumulate +
+	      0
+	      (map square
+		   (filter odd? (enumerate-tree tree))))
+  )
+
+
+(define (even-fibs n)
+  (accumulate cons '()
+	      (filter even? (map fib (enumerate-interval 0 n))))
+  )
+
+(define (map p sequence)
+  (accumulate (lambda (x y) (p x)) nil sequence)
+  )
